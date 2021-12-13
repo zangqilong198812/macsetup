3b030cc2f36c89271a8bc91ac2b7a6350f8f61e9
zang qilong <zangqilong@gmail.com>
2021-12-13 09:48:16 +0800

    Initial Commit

---
+++ MacSetup/MacSetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacSetupApp.swift
+//  MacSetup
+//
+//  Created by zang qilong on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct MacSetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
